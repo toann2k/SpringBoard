4de1eb2d8547e8477d33d5e47e7b4f481a82398c
toann2k <136713949+toann2k@users.noreply.github.com>
2023-10-11 07:37:35 -0500

    Add files via upload

---
+++ WTForms/app.py
@@ -0,0 +1,68 @@
+
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+
+from models import db, connect_db, Pet
+from forms import AddPetForm, EditPetForm
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adapt"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '1234'
+
+
+connect_db(app)
+app.app_context().push()
+db.create_all()
+
+###############################################################################################
+
+@app.route("/")
+def list_pets():
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        if form.validate_on_submit():
+            data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+            new_pet = Pet(**data)
+            # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+            db.session.add(new_pet)
+            db.session.commit()
+            flash(f"{new_pet.name} added.")
+            return redirect(url_for('list_pets'))
+
+    else:
+        # re-present form for editing
+        return render_template("pet_add_form.html", form=form)
+    
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def edit_pet(pet_id):
+    """Edit pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+        flash(f"{pet.name} updated.")
+        return redirect(url_for('list_pets'))
+
+    else:
+        # failed; re-present form for editing
+        return render_template("pet_edit_form.html", form=form, pet=pet)
+
+@app.route("/api/pets/<int:pet_id>", methods=['GET'])
+def api_get_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    info = {"name":pet.name,"age":pet.age}
+    return jsonify(info)
+
